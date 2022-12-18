8d5fa68306b4d63d358149b5b2d19f8d5ba7b7f3
Sathvika-7284 <sathvika7276@gmail.com>
2022-12-18 12:53:59 +0800

    defining attributes and method in guitar

--- guitar.py
+++ guitar.py
@@ -1,7 +1,7 @@
 """
 Guitar
 Estimate: 30 minutes
-Actual:   40 minutes
+Actual:   15 minutes
 """
 CURRENT_YEAR = 2022
 VINTAGE_YEAR = 50
@@ -11,13 +11,13 @@ class Guitar:
     """ Storing details of guitar """
 
     def __init__(self, name="", year=0, cost=0):
-        """Construct guitar type from the given values."""
+        """ Construct guitar type from the given values."""
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
-        """Return string representation of a guitar."""
+        """ Return string representation of a guitar."""
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
 
     def get_age(self):
